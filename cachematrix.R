4956b4686ec7d4df956f7d5bbd5311ab903ea9f2
Alberto Olivieri <albertofedericoolivieri@gmail.com>
2018-04-03 18:09:15 +0200

    Assignment for COURSERA

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This FUN chache the results of a Solve function called over an invertible
+## matrix so, if the matix does not change, the operation is not computed again
+## and the result is recovered from the cache
 
-## Write a short comment describing this function
+## This firs FUN create a special matrix, set and get the values of the matix 
+## and of the solve FUN
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y){
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list ( set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This FUN Solve the invertible matrix and if a solution for the matrix already
+## exists the FUN get it from the makeCacheMatrix FUN instead of computing the
+## results again
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if (!is.null(s)){
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
